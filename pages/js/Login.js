dd7cfd0db013067e2efaf3c8bd6d0f78fdb4d796
MehdiEl <40770264+Mehdi2210@users.noreply.github.com>
2020-04-14 19:35:26 +0100

    Add files via upload

--- pages/js/Login.js
+++ pages/js/Login.js
@@ -6,3 +6,23 @@ function resetFilliere() {
     var s = document.getElementById("profil");
     s.selectedIndex = s.options[0];
 }
+function mdpIdentiques(form) {
+                mdp = form.mdp.value;
+                cmdp = form.cmdp.value;
+
+                if (mdp == '')
+                    alert ("Veuillez saisir un mot de passe");
+
+                else if (cmdp == '')
+                    alert ("Veuillez confirmer votre mot de passe");
+
+                else if (mdp != cmdp) {
+                    alert ("\n Veuillez saisir des mots de passe identiques")
+                    return false;
+                }
+
+                else{
+
+                    return true;
+                }
+            }
